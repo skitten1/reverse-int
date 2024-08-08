101be57e52e1b9a49a19f1b3cc0d799e55495254
skitten1 <skitten11@gmail.com>
2024-08-09 00:07:01 +0300

    dosc: add reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let str = Math.abs(n).toString();
+  let result = '';
+  let i = str.length - 1;
+  while (i >= 0) {
+    result += str[i];
+    i -= 1;
+  }
+  return (Number(result));
 }
